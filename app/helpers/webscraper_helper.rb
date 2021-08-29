8258532a6c09895f16680bd011c645b0cf364dbc
Daniel <dpericich@gmail.com>
2021-08-29 18:26:10 -0500

    yahoo_stock_scraper method and helper methods

--- app/helpers/webscraper_helper.rb
+++ app/helpers/webscraper_helper.rb
@@ -9,6 +9,19 @@ module WebscraperHelper
     DailyStock.create!(company_name: company_name, ticker_symbol: ticker_symbol, ending_price: ending_price, ending_date: ending_date)
   end
 
+  def self.yahoo_stock_scraper(ticker_symbol)
+    begin
+      parsed_web_page = retrieve_yahoo_page(ticker_symbol)
+      company_name = yahoo_company_name(parsed_web_page)
+      ticker_symbol = ticker_symbol
+      ending_price = yahoo_stock_price(parsed_web_page)
+      ending_date = Date.today
+
+      DailyStock.create!(company_name: company_name, ticker_symbol: ticker_symbol, ending_price: ending_price, ending_date: ending_date)
+    rescue => e
+      pp "Sorry, we can't find a record for the '#{ticker_symbol}' ticker symbol."
+    end
+  end
 
 
   def self.google_company_scraper(ticker_symbol)
@@ -32,6 +45,12 @@ module WebscraperHelper
     parsed_web_page = Nokogiri::HTML5(web_page)
   end
 
+  def self.retrieve_yahoo_page(ticker_symbol)
+    address="https://finance.yahoo.com/quote/#{ticker_symbol}"
+    web_page = HTTParty.get(address)
+    parsed_web_page = Nokogiri::HTML(web_page)
+  end
+
   def self.format_google_company_name(parsed_web_page)
     company_name = parsed_web_page.css('div.BNeawe.deIvCb.AP7Wnd').text
     company_name = company_name.downcase.split('top')[0].titleize
@@ -43,5 +62,14 @@ module WebscraperHelper
     # Need to do something here for
   end
 
-  private_class_method :format_google_company_name, :format_google_ending_price
+  def self.yahoo_company_name(parsed_web_page)
+    parsed_web_page.css("h1").text.split("\(")[0].strip
+  end
+
+  def self.yahoo_stock_price(parsed_web_page)
+    ending_price = parsed_web_page.css("div>div>div>div>div>div>div>span")[3].text
+    ending_price = ending_price.delete(",")
+  end
+
+  private_class_method :format_google_company_name, :format_google_ending_price, :retrieve_yahoo_page, :yahoo_company_name, :yahoo_stock_price
 end
