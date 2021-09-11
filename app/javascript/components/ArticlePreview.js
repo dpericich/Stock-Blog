5754f747d5399fdd3f84ab5eebd79924bbb3624b
Daniel <dpericich@gmail.com>
2021-09-11 16:09:16 -0500

    updated prop threading for indvid article previews

--- app/javascript/components/ArticlePreview.js
+++ app/javascript/components/ArticlePreview.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ArticlePreview = ({articles, preview}) => {
+const ArticlePreview = ({title, author, date, preview, id}) => {
   return(
-    <div className="article-preview__container">
-      {articles.map((article) =>
-      <div className="article-preview__preview">
-        <h3 className="article-preview__heading">{article.title}</h3>
+    <div className="article-preview__preview">
+        <h3 className="article-preview__heading">{title}</h3>
         <div className="article-preview__publish">
-          <p className="article-preview__publish--author" >{article.author}</p>
-          {/* This is a sting => Need to get it to a date object to be able to manipulate it */}
-          <p className="article-preview__publish--date">{article.published_date}</p>
-          {console.log(article.published_date)}
+          <p className="article-preview__publish--author" >{author}</p>
+          <p className="article-preview__publish--date">{date}</p>
         </div>
-        <p className="article-preview__text">{article.preview}</p>
-        <a href={`/articles/${article.id}`} className="article-preview__article-link">Read More...</a>
+        <p className="article-preview__text">{preview}</p>
+        <a href={`/articles/${id}`} className="article-preview__article-link">Read More...</a>
       </div>
-      )}
-    </div>
   )
 }
 
